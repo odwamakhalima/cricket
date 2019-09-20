3ec7ba8104684cf88a294f0897c6fdc21334751e
Odwa Makhalima <3652687@myuwc.ac.za>
2019-09-20 14:53:01 +0200

    first com

--- cricketFact.js
+++ cricketFact.js
@@ -1,36 +1,80 @@
-module.exports = function crickets(){
-    var str ;
-    var list = ['-',1,2,3,4,6,'w']
+module.exports = function CricketScoreKeeper() {
+    var list = []
     var check;
     var newList = []
     var result;
+    var wicket = 10;
+    var overs = 0;
+    var checkwick;
+    var errorM
 
-    function add(){
-        for(var i =1;i<list.length;i++){
-           check = list[Math.floor(Math.random()*list.length)];
-            
-            newList.push(check)
-            //console.log(newList)
+    function error() {
+        return errorM
+    }
+
+    function setOver(set) {
+        overs = set
+
+    }
+    function oversLeft() {
+        return overs
+    }
+
+    function checkwicks() {
+        return wicket
+    }
+
+
+    function add(lists) {
+        list = lists.split('')
+        if (lists.length == 6) {
+            if (overs > 0) {
+                if (wicket > 0) {
+                    for (var i = 1; i < list.length; i++) {
+                        var n = list.includes("w");
+                        check = list[i];
+
+                        newList.push(check);
+                    }
+                    if (n == true) {
+                        wicket--
+
+                    }
+                    overs--
+                }
+            }
+        }
+        else{
+            errorM = 'Enter a valid score'
         }
     }
-    function score(){
-        result = newList.reduce((a, b) => a + b, 2)
-        console.log(result)
-         return result
+
+
+
+    function over() {
+        
+        return newList
     }
 
-    function addScore(newList){
-        var total = 0
-        for(var j=0;j<newList.length;j++){
-            total += newList[j]
-            
-        }
-        console.log(total)
+    function score() {
+    
+        let numOr0 = n => isNaN(n) ? 0 : n
+
+        result = newList.reduce((a, b) =>
+            Number(numOr0(a)) + Number(numOr0(b)))
+
+        return result
     }
 
-    return{
+
+    return {
         add,
         score,
-        addScore,
+        setOver,
+        over,
+        checkwicks,
+        oversLeft,
+        error
+
     }
 }
--- test/cricket.test.js
+++ test/cricket.test.js
@@ -1,22 +1,68 @@
 var assert = require('assert')
 
-var crickets = require('../cricketFact')
+var CricketScoreKeeper = require('../cricketFact')
 
-describe('The cricket function', function(){
-    var cricFact = crickets()
+describe('The cricket function', function () {
 
-    cricFact.add()
 
-    // it('should ', function(){
-    //     assert.equal(cricFact.score(), cricFact.score());
-    // });
-    // var cricFact = crickets()
+    it('should only return how many overs left ', function () {
+        var cricFact = CricketScoreKeeper()
 
-    // cricFact.add()
-    // cricFact.score()
+        
+    
+        cricFact.setOver(4)
+        cricFact.add('-123ww')
+        cricFact.add('-1234w')
+        cricFact.add('-1234w')
 
-    // it('should ', function(){
-    //     assert.equal(0, cricFact.addScore(cricFact.score()));
-    // });
+
+
+        assert.equal(1,cricFact.oversLeft() );
+    });
+
+    it('should get the total score in an over ', function(){
+        var cricFact = CricketScoreKeeper()
+      
+        cricFact.setOver(3)
+
+        cricFact.add('-1234w')
+        cricFact.add('-1234w')
+        cricFact.over()
+        
+
+        assert.equal(20, cricFact.score());
+    });
+
+    it('should return the wickets left ', function(){
+        var cricFact = CricketScoreKeeper()
+
+        cricFact.setOver(4)
+        
+
+        cricFact.add('--wwww')
+        cricFact.add('ww-www')
+        cricFact.add('3-w-41')
+        cricFact.over()
+        
+
+        assert.equal(7,cricFact.checkwicks() );
+    });
+    
+
+    it('if the length of the string is not equal to 6 should return an error ', function(){
+        var cricFact = CricketScoreKeeper()
+
+        cricFact.setOver(4)
+        
+
+    
+        cricFact.add('w-41')
+        cricFact.over()
+        
+
+        assert.equal('Enter a valid score',cricFact.error() );
+    });
+
+
 
 });
